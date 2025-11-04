4596aaef63ebc8cf54a24c4b9c1d0b93ed73f48c
Rohith Raju <107455049+Rr333333333@users.noreply.github.com>
2025-11-04 09:44:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+window.addEventListener('scroll', () => {
+  const nav = document.querySelector('header');
+  if (window.scrollY > 50) {
+    nav.style.background = '#0a0a0a';
+  } else {
+    nav.style.background = '#111';
+  }
+});
 
